a526d92e99266ee2f2a8ed5458b93612cce9ebb3
Abdullah Ejaz <abdullahejaz123789@gmail.com>
2026-05-02 12:37:08 +0500

    Forgot import

--- app/src/main/java/com/example/musicplayer/ProfileFragment.java
+++ app/src/main/java/com/example/musicplayer/ProfileFragment.java
@@ -5,6 +5,7 @@ import android.app.AlertDialog;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Base64;
